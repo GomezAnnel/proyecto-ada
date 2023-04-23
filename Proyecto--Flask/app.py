279b4683d56d65ce6a495fe4ad71936a2ad0bfb1
Hubert Gomez <annel.geminis@gmail.com>
2023-04-22 21:29:03 -0300

    Inicia proyecto flask

--- Proyecto--Flask/app.py
+++ Proyecto--Flask/app.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+
+        return render_template('home-banking.html',saludo = personal.nombre)
+
     
-    return render_template('home-banking.html')
 
 
 if __name__ == '__main__':
